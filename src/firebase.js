c5664e952211b0e48ad50f4bc03db001a39bc5af
Alex Gitlin <alex.gitlin@gmail.com>
2018-05-01 17:28:59 -0400

    cfg

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 
   import firebase from 'firebase';
+  import cfg from './cfg';
   const config = {
-    apiKey: "AIzaSyCP4muCuxOvY2ltRkjvr0P5yDWnARrCW1U",
-    authDomain: "zero-2-app.firebaseapp.com",
+    apiKey: cfg.API,
+    authDomain: cfg.AUTH,
     databaseURL: "https://zero-2-app.firebaseio.com",
     projectId: "zero-2-app",
     storageBucket: "",
-    messagingSenderId: "815631548492"
+    messagingSenderId: cfg.MSG
   };
   var fire=firebase.initializeApp(config);
 export const provider = new firebase.auth.GoogleAuthProvider();
